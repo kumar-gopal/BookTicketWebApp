620b8857132df92bbfd6701fd93f58b047eb6369
kumar-gopal <kumargopal782001@gmail.com>
2025-06-04 05:53:46 +0530

    api fix

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.get("/home", (req, res) => {
   });
 });
 
-app.get("/", (req, res) => {
+app.get("/health", (req, res) => {
   res.status(200).json({
     message: "If you are seeing this, it means the server is running successfully.",
     status: "success"
